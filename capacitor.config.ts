b57308f8a08b581a1f7dbbac8b42e5b691b8cf22
Fawaz Ahmad <fawaz.ahmad@digitaltentstudio.com>
2026-05-14 12:38:27 +0500

    feat: add splash screen with 4s duration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.citysahulat.app',
   appName: 'City Sahulat',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 4000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
